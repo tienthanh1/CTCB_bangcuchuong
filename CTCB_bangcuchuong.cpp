38b5b302ea7563a3226e9a29f0d4049fa92b47cb
tien thanh <tienthanh@gmail.com>
2024-03-04 11:12:12 +0700

    Add project files.

---
+++ CTCB_bangcuchuong.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int sodau, socuoi, i = 0;
+	cout << "nhap so:";
+	cin >> so;
+	for (int i = 0, i < 5, i++) {
+		if (i % 2 != 0);
+		cout << i << " ";
+		tongle = tongle + 1;
+	}cout << "\ntongcac so le ta o den " << so << "la" << tongle;
+	return
+}
